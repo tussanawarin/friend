897e9f642e02e5ea1afb5b97c9646f5c4de3a82b
tussanawarin <25993@srithatpit.ac.th>
2025-01-24 10:55:11 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,11 +1,34 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>HTMLเบื้องต้น</title>
+</head>
+<body>
+   <p>------การแสดงข้อความแบบหัวข้อ------</p>
+   <div>
+      <h1>HTMLเบื้องต้น(h1)</h1>
+   </div>
+   <div>
+      <h2>HTMLเบื้องต้น(h2)</h2>
+      <h3>HTMLเบื้องต้น(h3)</h3>
+      <h4>HTMLเบื้องต้น(h4)</h4>
+      <h5>HTMLเบื้องต้น(h5)</h5>
+      <h6>HTMLเบื้องต้น(h6)</h6>
+   </div>
+   <p>------การสร้างตัวหนา ตัวเอียง การขีดเส้นใต้ข้อความ------</p>
+   </div>
+      <b>ทัศนวฤนท์ โจมแก้ว</b><br>
+      <i>ทัศนวฤนท์ โจมแก้ว</i><br>
+      <u>ทัศนวฤนท์ โจมแก้ว</u><br>
+   </div>  
+      <p><b>-----การ LINK-----</b></p>
+      <div>
+      <!--link หน้าเดียว-->
+      <a href = "htts://www.google.co.th">Google</a><br>
+   </div>
+
+</body>
+</html>
